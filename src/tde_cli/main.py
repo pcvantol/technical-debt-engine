1011c45fe49863774635be2b61361f6f0e22eead
pcvantol <pcvantol@hotmail.com>
2026-07-28 17:17:11 +0200

    Prepare TDE 1.0.3 release

--- src/tde_cli/main.py
+++ src/tde_cli/main.py
@@ -29,7 +29,7 @@ from tde_runtime.repository_qualification import (QualificationRegistry,
 from tde_runtime.differential import AssessmentBaselineRegistry, DifferentialEngine, DifferentialError
 
 
-CLI_VERSION = "1.0.2"
+CLI_VERSION = "1.0.3"
 GENERATION = "1"
 
 
